92df8d56525f7e8ccff6b20686347d5516450e6e
DmitroOmelianenko <dmitroomelianenko@gmail.com>
2025-04-19 15:14:55 +0300

    base structer

---
+++ dist/goit-js-hw-06-parcel.6f9f29c0.js
@@ -0,0 +1,2 @@
+console.log("Hello world!");
+//# sourceMappingURL=goit-js-hw-06-parcel.6f9f29c0.js.map
---
+++ dist/goit-js-hw-06-parcel.816e7b21.js
@@ -0,0 +1,3 @@
+console.log('Hello world!');
+
+//# sourceMappingURL=goit-js-hw-06-parcel.816e7b21.js.map
